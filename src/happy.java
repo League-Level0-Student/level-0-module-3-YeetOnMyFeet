67c53af1f4b596966b5c4a91b78d7df2a4e59765
SMHS-519-13 <SMHS-519-13@SMHS-519-13.smhsrop.lab>
2020-02-20 16:26:49 -0800

    commmit

--- src/happy.java
+++ src/happy.java
@@ -1,4 +1,30 @@
+import javax.swing.JOptionPane;
 
 public class happy {
-
+public static void main(String[] args) {
+	
+	String firstq = JOptionPane.showInputDialog("Are you happy?");
+	String secondq = JOptionPane.showInputDialog("Do you want to be happy?");
+	if (firstq.equals("yes")) {
+		JOptionPane.showMessageDialog(null, "Keep doing what you are doing!");
+	}
+	if (firstq.equals("no")) {
+	String secondq = JOptionPane.showInputDialog("Do you want to be happy?");
+	}
+	if (secondq.equals("no")) {
+		JOptionPane.showMessageDialog(null, "Keep doing what you are doing!");
+	}
+	
+	
+	
+	
+	
+	
+	
+}
+	
+	
+	
+	
+	
 }
